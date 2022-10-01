4584f660016d286e698cd4c0925511dfd698afe5
Matthew Nichols <matthew@matthew-nichols.com>
2022-10-01 21:01:38 +0200

    Created basic picture page.

--- src/pages/_data/picturePages/parsed.js
+++ src/pages/_data/picturePages/parsed.js
@@ -1,6 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 
+const PicturePageImagePathBase = "/pictures/";
+
+class PicturePageImage {
+  constructor(imageData) {
+    this.pathName = imageData.path || null;
+    this.path = `${PicturePageImagePathBase}${imageData.path || ""}`;
+    this.title = imageData.title || null;
+  }
+}
+  class PicturePage {
+  constructor(data) {
+    this.pageName = data.pageName;
+    if (data.images && data.images.length) {
+      this.images = data.images.map((p) => new PicturePageImage(p));
+    }
+  }
+}
+
 module.exports = async function() {
   const dirFiles = await fs.promises.readdir(__dirname);
   const jsonFiles = dirFiles.filter((f) => f.endsWith("json5"));
@@ -9,5 +27,5 @@ module.exports = async function() {
     return require(path.join(__dirname, fileName));
   })
   
-  return pagesData
+  return pagesData.map((pd) => new PicturePage(pd));
 };
